ca8d98622bd64b27689ae7ae140cff7406ab343d
Ssabae <llsb156@gmail.com>
2019-01-06 15:20:52 +0900

    HATEOAS

--- src/main/java/com/ssabae/demo/events/EventController.java
+++ src/main/java/com/ssabae/demo/events/EventController.java
@@ -2,6 +2,7 @@ package com.ssabae.demo.events;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.hateoas.MediaTypes;
+import org.springframework.hateoas.mvc.ControllerLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.Errors;
@@ -45,7 +46,11 @@ public class EventController {
         Event event = modelMapper.map(eventDto, Event.class);
         event.update();
         Event newEvent = this.eventRepository.save(event);
-        URI createUri = linkTo(EventController.class).slash(newEvent.getId()).toUri();
-        return ResponseEntity.created(createUri).body(event);
+        ControllerLinkBuilder selfLinkBuilder = linkTo(EventController.class).slash(newEvent.getId());
+        URI createUri = selfLinkBuilder.toUri();
+        EventResource resource = new EventResource(event);
+        resource.add(linkTo(EventController.class).withRel("query-events"));
+        resource.add(selfLinkBuilder.withRel("update-event"));
+        return ResponseEntity.created(createUri).body(resource);
     }
 }
