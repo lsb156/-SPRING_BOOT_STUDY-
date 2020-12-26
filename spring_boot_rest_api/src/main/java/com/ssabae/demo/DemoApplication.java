e2096da45a5aec6de56f736915d272bd7f43e42f
Ssabae <llsb156@gmail.com>
2020-12-26 11:35:44 +0900

    필요없는 소스 삭제

--- spring_boot_rest_api/src/main/java/com/ssabae/demo/DemoApplication.java
+++ spring_boot_rest_api/src/main/java/com/ssabae/demo/DemoApplication.java
@@ -14,9 +14,6 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        JpaRepository
-//        PlatformTransactionManager
-//        AbstractPlatformTransactionManager
         SpringApplication.run(DemoApplication.class, args);
     }
 
